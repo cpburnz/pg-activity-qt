dce1b308fa2e768c1335ba9a908d15940e67aba5
cpburnz <2126043+cpburnz@users.noreply.github.com>
2022-10-04 00:43:03 -0400

    WIP

--- app/activity.py
+++ app/activity.py
@@ -6,27 +6,29 @@ get the activity information from PostgreSQL.
 import dataclasses
 import logging
 from typing import (
-	Optional)
+	Any,
+	List,
+	NamedTuple,
+	Optional,
+	Tuple)
 
 import psycopg2
 import psycopg2.extensions
 import psycopg2.extras
 from PySide6.QtCore import (
-	QObject,
-	QThreadPool,
-	Signal,
-	SignalInstance)
+	QThreadPool)
 
 from app.threads import (
-	Worker)
+	Worker,
+	WorkerFuture)
 
 LOG = logging.getLogger(__name__)
 """
 The module logger.
 """
 
-# TODO: I LEFT OFF HERE! 2022-10-02
-# - Impelement require methods.
+# TODO: Implement connection.
+
 
 class PostgresActivityModel(object):
 	"""
@@ -57,10 +59,55 @@ class PostgresActivityModel(object):
 		connection parameters.
 		"""
 
+		self.__pg_version: Optional[Tuple[int, int]] = None
+		"""
+		*__pg_version* (:class:`tuple` of :class:`int`) is the version of the
+		PostgreSQL database.
+		"""
+
 		self.__pool: QThreadPool = pool
 		"""
 		*__pool* (:class:`QThreadPool`) is the thread worker pool.
 		"""
+
+	def cancel_query(self, pid: int) -> WorkerFuture:
+		"""
+		Cancel the query.
+
+		*pid* (:class:`int`) is the PID of the backend process.
+
+		Returns a :class:`WorkerFuture`. On success, the emitted result will be
+		whether the process was terminated (:class:`True`), or not (:class:`False`).
+		"""
+		LOG.debug("Cancel query.")
+		conn = self.__conn
+		worker = Worker(lambda: self.__cancel_query_work(conn, pid))
+		future = worker.make_future()
+		self.__pool.start(worker)
+		return future
+
+	@staticmethod
+	def __cancel_query_work(
+		conn: psycopg2.extensions.connection,
+		pid: int,
+	) -> bool:
+		"""
+		Run the cancel query.
+
+		*conn* (:class:`psycopg2.extensions.connection`) is the PostgreSQL
+		connection.
+
+		*pid* (:class:`int`) is the PID of the backend process.
+
+		Returns whether the process was terminated (:class:`True`), or not
+		(:class:`False`).
+		"""
+		cursor = conn.cursor()
+		cursor.execute("""
+			SELECT pg_cancel_backend(%(pid)s) AS success;
+		""", {'pid': pid})
+		row: _QueryCancelRow = cursor.fetchone()
+		return row.success
 
 	def close(self) -> None:
 		"""
@@ -70,7 +117,6 @@ class PostgresActivityModel(object):
 		if self.__conn is not None:
 			conn, self.__conn = self.__conn, None
 			worker = Worker(lambda: self.__close_work(conn))
-
 			self.__pool.start(worker)
 
 	@staticmethod
@@ -84,6 +130,157 @@ class PostgresActivityModel(object):
 		conn.rollback()
 		conn.close()
 
+	def fetch_activity(self) -> WorkerFuture:
+		"""
+		Fetch the current activity from PostgreSQL.
+
+		Returns a :class:`WorkerFuture`. On success, the emitted result will be a
+		:class:`list` of :class:`ActivityRow`.
+		"""
+		LOG.debug("Fetch activity.")
+		conn = self.__conn
+
+		if self.__pg_version >= (9, 2):
+			worker = Worker(lambda: self.__fetch_activity_work_ge_92(conn))
+		else:
+			worker = Worker(lambda: self.__fetch_activity_work_le_91(conn))
+
+		future = worker.make_future()
+		self.__pool.start(worker)
+		return future
+
+	@staticmethod
+	def __fetch_activity_work_ge_92(
+		conn: psycopg2.extensions.connection,
+	) -> List['ActivityRow']:
+		"""
+		Run the fetch activity query for PostgreSQL 9.2 and above.
+
+		*conn* (:class:`psycopg2.extensions.connection`) is the PostgreSQL
+		connection.
+
+		Returns the activity (:class:`list` of :class:`ActivityRow`).
+		"""
+		cursor = conn.cursor()
+		cursor.execute("""
+			SELECT
+				application_name,
+				backend_start,
+				client_addr,
+				nullif(client_hostname, '') AS client_hostname,
+				client_port,
+				datname,
+				pid,
+				query_start,
+				state,
+				state_change,
+				usename,
+				waiting,
+				xact_start
+			FROM pg_stat_activity;
+		""")
+		return cursor.fetchall()  # type: ignore
+
+	@staticmethod
+	def __fetch_activity_work_le_91(
+		conn: psycopg2.extensions.connection,
+	) -> List['ActivityRow']:
+		"""
+		Run the fetch activity query for PostgreSQL 9.1 and below.
+
+		*conn* (:class:`psycopg2.extensions.connection`) is the PostgreSQL
+		connection.
+
+		Returns the activity (:class:`list` of :class:`ActivityRow`).
+		"""
+		cursor = conn.cursor()
+		cursor.execute("""
+			SELECT
+				application_name,
+				backend_start,
+				client_addr,
+				client_hostname,
+				client_port,
+				datname,
+				procpid AS pid,
+				query_start,
+				(CASE
+					WHEN current_query = '<IDLE>'
+						THEN 'idle'
+					WHEN current_query = '<IDLE> in transaction'
+						THEN 'idle in transaction'
+					WHEN current_query = '<IDLE> in transaction (aborted)'
+						THEN 'idle in transaction (aborted)'
+					ELSE
+						(CASE WHEN current_query LIKE '<IDLE>%'
+							THEN current_query
+							ELSE 'active'
+						END)
+				END) AS state,
+				NULL::text AS state_change,
+				usename,
+				waiting,
+				xact_start
+			FROM pg_stat_activity;
+		""")
+		return cursor.fetchall()  # type: ignore
+
+	def terminate_query(self, pid: int) -> WorkerFuture:
+		"""
+		Terminate the query.
+
+		*pid* (:class:`int`) is the PID of the backend process.
+
+		Returns a :class:`WorkerFuture`. On success, the emitted result will be
+		whether the process was terminated (:class:`True`), or not (:class:`False`).
+		"""
+		LOG.debug("Terminate query.")
+		conn = self.__conn
+		worker = Worker(lambda: self.__terminate_query_work(conn, pid))
+		future = worker.make_future()
+		self.__pool.start(worker)
+		return future
+
+	@staticmethod
+	def __terminate_query_work(
+		conn: psycopg2.extensions.connection,
+		pid: int,
+	) -> bool:
+		"""
+		Run the terminate query.
+
+		*conn* (:class:`psycopg2.extensions.connection`) is the PostgreSQL
+		connection.
+
+		*pid* (:class:`int`) is the PID of the backend process.
+
+		Returns whether the process was terminated (:class:`True`), or not
+		(:class:`False`).
+		"""
+		cursor = conn.cursor()
+		cursor.execute("""
+			SELECT pg_terminate_backend(%(pid)s) AS success;
+		""", {'pid': pid})
+		row: _QueryTerminateRow = cursor.fetchone()
+		return row.success
+
+
+class ActivityRow(NamedTuple):
+	# TODO: Type hint these.
+	application_name: Any  # TODO
+	backend_start: Any  # TODO
+	client_addr: Any  # TODO
+	client_hostname: Optional[str]
+	client_port: int
+	datname: str
+	pid: int
+	query_start: Any  # TODO
+	state: Any  # TODO
+	state_change: Any  # TODO
+	usename: Any  # TODO
+	waiting: Any  # TODO
+	xact_start: Any  # TODO
+
 
 @dataclasses.dataclass(frozen=True)
 class PostgresConnectionParams(object):
@@ -92,3 +289,15 @@ class PostgresConnectionParams(object):
 	password: str
 	port: int
 	user: str
+
+
+class _QueryCancelRow(NamedTuple):
+	success: bool
+
+
+class _QueryFetchRow(NamedTuple):
+	query: str
+
+
+class _QueryTerminateRow(NamedTuple):
+	success: bool
--- app/threads.py
+++ app/threads.py
@@ -3,15 +3,24 @@ This module defines utilities for dealing with threads from Qt.
 """
 
 import dataclasses
+import logging
 import traceback
 from typing import (
-	Callable)
+	Any,
+	Callable,
+	List,
+	Optional)
 
 from PySide6.QtCore import (
 	QObject,
 	QRunnable,
 	Signal,
 	SignalInstance)
+
+LOG = logging.getLogger(__name__)
+"""
+The module logger.
+"""
 
 
 class Worker(QRunnable):
@@ -38,6 +47,17 @@ class Worker(QRunnable):
 		*signals* (:class:`WorkerSignals`) contains the signals used by the worker.
 		"""
 
+	def make_future(self) -> 'WorkerFuture':
+		"""
+		Create a future for this worker.
+
+		Returns the future (:class:`WorkerFuture`).
+		"""
+		future = WorkerFuture()
+		self.signals.result.connect(future.set_result)
+		self.signals.error.connect(future.set_error)
+		return future
+
 	def run(self) -> None:
 		"""
 		Runs the worker.
@@ -53,6 +73,117 @@ class Worker(QRunnable):
 			self.signals.error.emit(error)
 		else:
 			self.signals.result.emit(result)
+
+
+class WorkerFuture(object):
+	"""
+	The :class:`WorkerFuture` class is used to encapsulate the asynchronous
+	execution of a worker.
+	"""
+
+	def __init__(self) -> None:
+		"""
+		Initializes the :class:`WorkerFuture` instance.
+		"""
+
+		self.__error: Optional[WorkerError] = None
+		"""
+		*__error* (:class:`WorkerError` or :data:`None`) is the set error.
+		"""
+
+		self.__error_callbacks: List[Callable[[WorkerError], None]] = []
+		"""
+		*__error_callbacks* (:class:`list` of :class:`callable`) contains the
+		registered error callbacks.
+		"""
+
+		self.__has_result = False
+		"""
+		*__has_result* (:class:`bool`) is whether the result has been set.
+		"""
+
+		self.__result = None
+		"""
+		*__result* is is the set result.
+		"""
+
+		self.__result_callbacks: List[Callable[[Any], None]] = []
+		"""
+		*__result_callbacks* (:class:`list` of :class:`callable`) contains the
+		registered result callbacks.
+		"""
+
+	def add_error_callback(
+		self,
+		callback: Callable[['WorkerError'], None],
+	) -> None:
+		"""
+		Register a callback to be called when the worker fails with an error.
+
+		*callback* (:class:`callable`) is the error handler. On failure, this will
+		be called with the error (:class:`WorkerError`). The return value is
+		ignored.
+		"""
+		self.__error_callbacks.append(callback)
+		if self.__error is not None:
+			self.__invoke_error_callbacks()
+
+	def add_result_callback(
+		self,
+		callback: Callable[[Any], None],
+	) -> None:
+		"""
+		Register a callback to be called when the worker succeeds.
+
+		*callback* (:class:`callable`) is the result handler. On success, this will
+		be called with the worker result. The return value is ignored.
+		"""
+		self.__result_callbacks.append(callback)
+		if self.__has_result:
+			self.__invoke_result_callbacks()
+
+	def __invoke_error_callbacks(self) -> None:
+		"""
+		Invoke the registered error callbacks.
+		"""
+		assert self.__error is not None, f"Error {self.__error!r} is not set"
+		while self.__error_callbacks:
+			callback = self.__error_callbacks.pop(0)
+			try:
+				callback(self.__error)
+			except:  # noqa
+				LOG.exception(f"Unhandled exception in error callback {callback!r}.")
+
+	def __invoke_result_callbacks(self) -> None:
+		"""
+		Invoke the registered result callbacks.
+		"""
+		assert self.__has_result, f"Result {self.__has_result!r} is not set."
+		while self.__result_callbacks:
+			callback = self.__result_callbacks.pop(0)
+			try:
+				callback(self.__result)
+			except:  # noqa
+				LOG.exception(f"Unhandled exception in result callback {callback!r}.")
+
+	def set_error(self, error: 'WorkerError') -> None:
+		"""
+		Set the error of the future.
+
+		*error* (:class:`WorkerError`) is the error to set.
+		"""
+		self.__error = error
+		self.__invoke_error_callbacks()
+
+	def set_result(self, result: Any) -> None:
+		"""
+		Set the result of the future.
+
+		*result* is the value to set.
+		"""
+		self.__has_result = True
+		self.__result = result
+		self.__invoke_result_callbacks()
 
 
 class WorkerSignals(QObject):
